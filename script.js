a581c12d81d91a45d0c5f0bccc1a2c7344c1d1b7
Sanjay-Aski <sanjayaski6553@gmail.com>
2024-12-06 18:04:51 +0530

    No much changes

--- script.js
+++ script.js
@@ -16,51 +16,54 @@ function nameplat() {
 }
 setInterval(nameplat, 100);
 
-
 var skill = document.getElementById("skills");
 var img_container = document.getElementById("img_container");
-var pic_file = ['java','mysql','Django','python','connection','html','css','js'];
+var pic_file = ['java', 'mysql', 'Django', 'python', 'connection', 'html', 'css', 'js'];
 
 var Pic_arrange = () => {
-    if(img_container.innerHTML != ""){
+    if (img_container.innerHTML !== "") {
         img_container.innerHTML = "";
     }
     let i = 0;
     let intervalId = setInterval(() => {
-        const img = document.createElement('img'); 
-        const img_src = '../Profile/pictures/Skill/' + pic_file[i] + '.png'; 
-        img.src = img_src; 
+        const img = document.createElement('img');
+        const img_src = '../Profile/pictures/Skill/' + pic_file[i] + '.png';
+        img.src = img_src;
         img.alt = pic_file[i];
-        img.classList.add(pic_file[i]); 
-        img.style.height = '8rem' ;
-        img.style.width = 'auto' ;
+        img.id = pic_file[i];
+        img.style.width = 'auto';
+        img.className = "skill-img";
         img_container.appendChild(img);
         i++;
-        if(i == pic_file.length) {
+        if (i === pic_file.length) {
             clearInterval(intervalId);
         }
     }, 100);
-}
+
+    const skill_win = document.querySelector("#skill");
+    const height = pic_file.length * 100; 
+    skill_win.style.height = height + "px";
+};
+
 const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             img_container.innerHTML = "";
             Pic_arrange();
-        }
-        else{
+        } else {
             img_container.innerHTML = "";
         }
     });
 }, { threshold: 0.2 });
 
-
 window.addEventListener('scroll', () => {
     const skillSection = document.getElementById("skill");
     const boundingRect = skillSection.getBoundingClientRect();
-    
+
     if (boundingRect.top > window.innerHeight || boundingRect.bottom < 0) {
         img_container.innerHTML = "";
     }
 });
+
 const skillSection = document.getElementById("skill");
 observer.observe(skillSection);
